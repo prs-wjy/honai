6c012c4a9899f43e5313efffed94b1f9d2ff6028
prs-wjy <prs.wjy@gmail.com>
2022-11-04 19:52:07 +0800

    init

---
+++ tes.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+  <title>Honai Resort</title>
+  <link rel="stylesheet" href="tes.css" />
+</head>
+<body>
+<div class="container">
+  <img src="a.jpg">
+  <img src="b.jpg">
+  <img src="b.jpg">
+  <img src="b.jpg">
+  <img src="b.jpg">
+  <img src="b.jpg">
+  <img src="b.jpg">
+  <img src="b.jpg">
+</div>
+</body>
+</html>
